23e9ef154ab5c0afc71dc9284af2c907f3bc3d61
steven-felim <stevenfelim04@gmail.com>
2025-05-22 20:42:03 +0700

    change session into jwt

--- middlewares/errorHandlerMiddleware.ts
+++ middlewares/errorHandlerMiddleware.ts
@@ -3,6 +3,7 @@ export function errorHandlerMiddleware(
   error: Error,
   req: Request,
   res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ) {
   res.status(500).json({ message: error.message });
