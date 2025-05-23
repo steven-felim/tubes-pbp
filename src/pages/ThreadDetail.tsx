3485d03d819d3bdae51b2d5baefc3ed421d4291f
steven-felim <stevenfelim04@gmail.com>
2025-05-23 10:53:26 +0700

    minor fix on nav bar

--- src/pages/ThreadDetail.tsx
+++ src/pages/ThreadDetail.tsx
@@ -125,9 +125,6 @@ const ThreadDetail = () => {
       <nav className="bg-gray-800 shadow-md">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
-            <Link to="/" className="text-white text-xl font-semibold">
-              ForumKode
-            </Link>
             <div className="flex items-center">
               <Link to="/" className="text-white text-xl font-semibold">
                 ForumKode
