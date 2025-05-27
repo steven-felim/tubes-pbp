1e248dfa3f9cf85ed712205a05220528cd7b32e1
steven-felim <stevenfelim04@gmail.com>
2025-05-27 15:12:03 +0700

    sign in and sign up using database

---
+++ migrations/20250527073049-create-user.js
@@ -0,0 +1,38 @@
+'use strict';
+
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.createTable('User', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: Sequelize.UUID,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+  });
+}
+
+export async function down(queryInterface) {
+  await queryInterface.dropTable('User');
+}
