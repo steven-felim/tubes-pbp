aaaf25ca34d72800eee111b6cea78bb64b0938a9
steven-felim <stevenfelim04@gmail.com>
2025-05-28 14:24:39 +0700

    fix bugs in creating thread

--- models/ThreadCategory.ts
+++ models/ThreadCategory.ts
@@ -1,13 +1,15 @@
-import {  Column,  DataType,  ForeignKey,  Model,  Table,} from "sequelize-typescript";
+import { Column,  DataType,  ForeignKey,  Model,  Table, PrimaryKey } from "sequelize-typescript";
 import { Thread } from "./Thread";
 import { Category } from "./Category";
 
 @Table({ tableName: "ThreadCategory", timestamps: false })
 export class ThreadCategory extends Model {
+  @PrimaryKey
   @ForeignKey(() => Thread)
   @Column(DataType.UUID)
   declare threadId: string;
 
+  @PrimaryKey
   @ForeignKey(() => Category)
   @Column(DataType.STRING)
   declare categoryName: string;
